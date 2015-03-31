a22bbff866357cf736bbb882503b3ad6ff201898
Sebastian Godelet <sebastian.godelet@outlook.com>
2015-03-31 08:50:15 +0800

    HACK: do not use dpn0 for filename

--- jssc2.cmd
+++ jssc2.cmd
@@ -1,7 +1,7 @@
 @setlocal enabledelayedexpansion enableextensions
 @for /F "usebackq" %%G in (`git rev-parse --short HEAD 2^>nul ^|^| echo UNVERSIONED`) do @set ID=%%G
 @set EXE=%TEMP%\_jssc_%ID%.exe
-if not exist "%EXE%" (
-    jsc /codepage:65001 /nologo /fast+ /out:"%EXE%" "%~dpn0.jss"
+@if not exist "%EXE%" (
+    jsc /codepage:65001 /nologo /fast+ /out:"%EXE%" "%~dp0jssc.jss"
 )
-"%EXE%" %*
+@call "%EXE%" %*
